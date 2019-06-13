95d66de75a115210543636ea02d4053d41722584
Robert Wade <robertwade@Roberts-MacBook-Pro.local>
2019-06-13 13:26:57 +0200

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout">
+    <header class="header">
+    </header>
+    <!-- Pages get injected in Slot -->
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
+
+</style>
---
+++ src/pages/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>TESTPAGE</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Whatuuup'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
